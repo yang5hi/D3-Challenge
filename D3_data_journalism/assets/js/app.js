c8af8fdf08eca66dc46d2d2847a60f3bc1f0a2ef
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-05-14 09:40:40 -0400

    d3 read csv

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,26 @@
+// Load data from data/data.csv
+d3.csv("assets/data/data.csv").then(function(censusData){
+    console.log(censusData);
+    // Log a list of state abbrs
+    const abbrs= censusData.map(data=>data.abbr);
+    console.log (abbrs);
+    // log Lacks healthcare and in poverty
+    const healthcareLacks=censusData.map(data => data.healthcare);
+    console.log (healthcareLacks);
+    const inPoverty=censusData.map(data => data.poverty);
+    console.log (inPoverty);
+    // log age median and smokes percentage
+    const ageMedian=censusData.map(data => data.age);
+    console.log (ageMedian);
+    const smokesPct=censusData.map(data => data.smokes);
+    console.log (smokesPct);
+    // log obesity percentage and median household income
+    const obesePct=censusData.map(data => data.obesity);
+    console.log (obesePct);
+    const incomeMedian=censusData.map(data => data.income);
+    console.log (incomeMedian);
+});
+
+function scatterPlot () {
+    Plotly.newPlot
+};
